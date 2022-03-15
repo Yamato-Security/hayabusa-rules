cff37fd06ddd0759de49110b64d37273d8e1fca9
itiB <is0312vx@ed.ritsumei.ac.jp>
2022-03-16 01:02:47 +0900

    Use windows-services.yml

--- tools/sigmac/convert.sh
+++ tools/sigmac/convert.sh
@@ -1,3 +1,3 @@
 rm -rf hayabusa_rules
-python ./tools/sigmac -t hayabusa --config ./tools/config/generic/sysmon.yml --defer-abort -r rules/windows/ > sigma_to_hayabusa.yml
+python ./tools/sigmac -t hayabusa --config ./tools/config/generic/windows-services.yml --defer-abort -r rules/windows/ > sigma_to_hayabusa.yml
 python splitter.py
