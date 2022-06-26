ce5fbe591ca673017f80dd0ae8756fcb7f21930f
itiB <is0312vx@ed.ritsumei.ac.jp>
2022-06-27 00:40:53 +0900

    Add: submodule for sigmac

--- tools/sigmac/convert.py
+++ tools/sigmac/convert.py
@@ -1,16 +1,17 @@
 import os
+import subprocess
 import shutil
 import ruamel.yaml
 import logging
-from typing import Dict, Set
+from typing import Dict, Set, List, Any
 
 FORMAT = ('[%(levelname)-8s] %(name)s, %(lineno)d: %(message)s')
 logging.basicConfig(format = FORMAT, level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 yaml = ruamel.yaml.YAML()
-sigma_dir = "."
-sigmac = "tools/sigma/sigmac.py"
+sigma_dir = "~/git/sigma"
+sigmac = "tools/sigmac"
 hayabusa_rule_path = "./hayabusa_rules"
 
 def main():
@@ -25,6 +26,8 @@ def main():
     logconverter.convert_rules()
 
 class Logconverter():
+    rule_count = 0
+
     def __init__(self, sigma_dir: str, rules_dir: str, config_dir: str):
         self.config_map: Dict[str, Set[str]] = dict()
 
@@ -37,7 +40,7 @@ class Logconverter():
         for config in configs:
             if not config.endswith(".yml"):
                 continue
-            if config == "windows-service.yml":
+            if config == "windows-services.yml":
                 continue
             with open(os.path.join(self.config_dir, config), 'r') as yml:
                 config_data = yaml.load(yml)
@@ -48,16 +51,31 @@ class Logconverter():
                             self.config_map[category] = set()
                         self.config_map[category].add(config)
 
-    def convert_rules(self, rules_dir=None):
-        rules_dir = rules_dir or self.rules_dir
+    def convert_rules(self):
+        self.convert_process = []
+        # TODO: 呼び出しを並行に
+        self._convert_rules(self.rules_dir)
+        for (p, output_path) in self.convert_process:
+            try:
+                p.wait(10)
+                with open(output_path, mode="w") as f:
+                    for l in p.stdout:
+                        l = l.decode("utf-8").strip()
+                        f.write(l + '\n')
+            except subprocess.TimeoutExpired:
+                logger.error("failed to convert " + output_path)
+            except Exception as err:
+                logger.error(err)
+
+    def _convert_rules(self, rules_dir):
         for file in os.listdir(rules_dir):
             file_path = os.path.join(rules_dir, file)
             if os.path.isdir(file_path):
-                self.convert_rules(file_path)
+                self._convert_rules(file_path)
             elif file.endswith(".yml"):
-                self.convert_rule(file_path, file)
+                self._convert_rule(file_path, file)
 
-    def convert_rule(self, rule_path: str, file_name: str):
+    def _convert_rule(self, rule_path: str, file_name: str):
         with open(rule_path, 'r') as yml:
             rule_data = yaml.load(yml)
             if "logsource" in rule_data and "category" in rule_data["logsource"]:
@@ -82,27 +100,52 @@ class Logconverter():
         rule_type = tmp[:off]
         path_from_off = tmp[off+1:]
 
+        # TODO: configs[i] に変更する
         if len(configs) > 0:
             for config in configs:
                 output_path = os.path.join(hayabusa_rule_path, rule_type + "_" + config[:-4], path_from_off)
                 output_dir = output_path[:-len(file_name)]
                 logger.debug("  output_path: " + output_path)
+                if not os.path.exists(output_dir):
+                    os.makedirs(output_dir)
 
-                command = "python3 " + os.path.join(sigma_dir, sigmac) + "-t hayabusa -c" + \
-                    os.path.join(self.config_dir, config) + "-c" + os.path.join(self.config_dir, "windows-service.yml") + \
-                    "--defer-abort -f " + rule_path
-                logger.debug("      command: " + command)
-                # TODO: call subprocess
+                sigma_command = [
+                    "python3",
+                    os.path.join(sigma_dir, sigmac),
+                    "-t",
+                    "hayabusa",
+                    "-c",
+                    os.path.join(self.config_dir, config),
+                    "-c",
+                    os.path.join(self.config_dir, "windows-services.yml"),
+                    "--defer-abort",
+                    rule_path
+                ]
+                logger.debug("  command: " + str(sigma_command))
+                proc = subprocess.Popen(sigma_command, stdout=subprocess.PIPE)
+                self.convert_process.append((proc, output_path))
+                self.rule_count += 1
         else:
             output_path = os.path.join(hayabusa_rule_path, rule_type, path_from_off)
             output_dir = output_path[:-len(file_name)]
             logger.debug("  output_path: " + output_path)
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir)
 
-            command = "python3 " + os.path.join(sigma_dir, sigmac) + "-t hayabusa -c" + \
-                os.path.join(self.config_dir, "windows-service.yml") + "--defer-abort -f " + rule_path
-            logger.debug("     command: " + command)
-            # TODO: call subprocess
-
+            sigma_command = [
+                "python3",
+                os.path.join(sigma_dir, sigmac),
+                "-t",
+                "hayabusa",
+                "-c",
+                os.path.join(self.config_dir, "windows-services.yml"),
+                "--defer-abort",
+                rule_path
+            ]
+            logger.debug("  command: " + str(sigma_command))
+            proc = subprocess.Popen(sigma_command, stdout=subprocess.PIPE)
+            self.convert_process.append((proc, output_path))
+            self.rule_count += 1
 
 if __name__ == "__main__":
     main()
