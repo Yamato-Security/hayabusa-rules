4e93edb586db6ac8b3ec025c4bb27cd7d4d231ef
fukusuket <41001169+fukusuket@users.noreply.github.com>
2023-12-18 04:05:49 +0900

    fix: common field

--- tools/sigmac/logsource_mapping.py
+++ tools/sigmac/logsource_mapping.py
@@ -141,7 +141,7 @@ class LogSource:
         return False
 
     def is_detectable_fields(self, keys) -> bool:
-        common_fields = ["CommandLine", "ProcessId", "OriginalFileName"]
+        common_fields = ["CommandLine", "ProcessId"]
         keys = [re.sub(r"\|.*", "", k) for k in keys]
         keys = [k for k in keys if k not in common_fields]
         if not keys:
